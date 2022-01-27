758d6cf422318695aa259b4c8092518b18293f06
Jeff Mitchell <sentinel1909@protonmail.com>
2022-01-26 21:07:14 -0800

    Fixed up code block style, content update

--- src/content/markdown.js
+++ src/content/markdown.js
@@ -8,8 +8,8 @@ const markdown = `# Rust Learning Notes
 ~~~
 - semi-colons are mandatory
 
-## Variables and Mutability
-### Variables
+## Variables, Constants, Mutability, and Shadowing
+
 - bind a value to a variable with the let keyword:
 ~~~Rust
 let x = 5;
@@ -19,8 +19,12 @@ let x = 5;
 ~~~Rust
 let mut x = 5;
 ~~~
+- variables can be shadowed, allowing re-use of the same name, which allows for transformations of a variable value
+~~~Rust
+let x = 10;
+let x = x + 5;      // shadow the variable named x and add 5 to it's value
+~~~
 
-### Constants
 - bind a value to a constant with the const keyword:
 ~~~Rust
 const c = 299792458;
@@ -33,6 +37,17 @@ const THREE_HOURS_IN_SECONDS: u32 = 60 * 6 * 3;
 - you can't use the mut keyword with constants
 - convention is to use uppercase names for constants
 - valid for the entire time the program runs, within the scope they were declared in
+
+## Data Types and Operations
+- Rust is a statically typed language, meaning it must know the types of all data at compile time
+- two core data types: scalar and compound
+- Rust supports basic mathematical operations such as addition, subtraction, division and remainder
+
+### Scalar Data Types
+- integer, floating point, boolean and char
+
+### Compound Data Types
+- tuples and arrays
 `;
 
 export default markdown;
